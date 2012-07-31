09c6c194244506e4e46ba5c6b2eebf585f5cbda3
Jan Dolecek <juzna.cz@gmail.com>
2012-07-31 13:47:03 +0200

    use VisualPaginator

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -9,6 +9,11 @@ class HomepagePresenter extends BasePresenter
 	public function renderDefault()
 	{
 		$this->template->anyVariable = 'any value';
+
+		$vp = new VisualPaginator($this, 'vp');
+		$pag = $vp->getPaginator();
+		$pag->itemsPerPage = 10;
+		$pag->itemCount = 51;
 	}
 
 }
