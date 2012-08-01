a414996fc8c06efd1f215e59665b4be4b8b7e39c
Jan Dolecek <juzna.cz@gmail.com>
2012-08-01 23:56:35 +0200

    fixed autoload

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,7 +7,7 @@ use Nette\Application\Routers\Route;
 
 
 // Load Nette Framework
-require LIBS_DIR . '/Nette/loader.php';
+require LIBS_DIR . '/autoload.php';
 
 
 // Configure application
@@ -21,7 +21,6 @@ $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 $configurator->createRobotLoader()
 	->addDirectory(APP_DIR)
-	->addDirectory(LIBS_DIR)
 	->register();
 
 // Create Dependency Injection container from config.neon file
